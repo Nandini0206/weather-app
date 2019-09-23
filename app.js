7e65c628a96cd637bab805fdd998a131f15341d2
Nandini Patel <nandini_patel96@hotmail.co.uk>
2019-09-23 11:10:45 +0100

    define long and lat variables to enable location

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-window.addEventListener('load', function() {
-  let long;
+window.addEventListener("load", function() {
   let lat;
+  let long;
+
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(position => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+    });
+  }
 });
