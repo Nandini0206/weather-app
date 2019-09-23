3003b59a15b1605f0852137fd0c0fca980c3a8f5
Nandini Patel <nandini_patel96@hotmail.co.uk>
2019-09-23 11:23:00 +0100

    correct error of underfined api

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ window.addEventListener("load", function() {
       lat = position.coords.latitude;
 
       const api = `https://api.darksky.net/forecast/55a4729d227ab7ca1bcc05dcbd75ff69/${lat},${long}`;
-    });
 
-    fetch(api)
-      .then(response => {
-        return response.json();
-      })
+      fetch(api)
+        .then(response => {
+          return response.json();
+        })
+        .then()
+    });
   }
 });
