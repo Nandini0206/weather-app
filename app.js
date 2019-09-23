068f52d13da397002b7e1248932c0497f9e0019b
Nandini Patel <nandini_patel96@hotmail.co.uk>
2019-09-23 11:17:22 +0100

    created constant to assign api to

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ window.addEventListener("load", function() {
     navigator.geolocation.getCurrentPosition(position => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
+
+      const api = `https://api.darksky.net/forecast/55a4729d227ab7ca1bcc05dcbd75ff69/37.8267,-122.4233`
     });
   }
 });
